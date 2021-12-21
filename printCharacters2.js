045a40d9d088fd1027329571c120f880b050ce59
Desislava Kracheva <desislava.92@abv.bg>
2021-12-21 10:28:51 +0200

    solve for 2nd time

---
+++ printCharacters2.js
@@ -0,0 +1,8 @@
+function solveForSecondTime(str){
+
+ for(let el of str){
+     console.log(el);
+ }
+
+}
+solveForSecondTime('Sentence')
---
+++ substring2.js
@@ -0,0 +1,7 @@
+function solveForSecondTime(str,num1,num2){
+
+    let substring = str.substr(num1, num2);
+    console.log(substring);
+
+}
+solveForSecondTime('ASentance', 1, 8)
